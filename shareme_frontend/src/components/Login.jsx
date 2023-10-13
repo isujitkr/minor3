4059476f2fa98b54e5d1a43d2d6671f94cb53fe5
Sujit Kumar <skr87981@gmail.com>
2023-10-14 00:51:53 +0530

    changes in login

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,38 +1,40 @@
 import React from "react";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import jwt_decode from "jwt-decode";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
+
 import { client } from "../client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const handleFailure = (error) => {
-    console.error("Google authentication failed:", error);
+  const onSuccess = (response) => {
+    console.log("LOGIN SUCCESS! response: ", response.credential);
+
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+
+    localStorage.setItem("user", JSON.stringify(userObject));
+
+    const { name, googleId, imageUrl } = userObject;
+
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
   };
 
-  const handleSuccess = (response) => {
-    console.log(response);
-    if (response && response.profileObj) {
-      const { name, googleId, imageUrl } = response.profileObj;
-
-      localStorage.setItem("user", JSON.stringify(response.profileObj));
-
-      const doc = {
-        _id: googleId,
-        _type: "user",
-        userName: name,
-        image: imageUrl,
-      };
-
-      client.createIfNotExists(doc).then(() => {
-        navigate("/", { replace: true });
-      });
-    } else {
-      console.error("Profile information not found in the response", response);
-    }
+  const onFailure = (response) => {
+    console.log("LOGIN FAILED! response: ", response);
   };
 
   return (
@@ -47,22 +49,31 @@ const Login = () => {
           autoPlay
           className="w-full h-full object-cover"
         />
+
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
           <div className="p-5">
             <img src={logo} width="130px" alt="logo" />
           </div>
-          <div className="shadow-2xl">
+
+          <div className="shadow-2x1">
             <GoogleOAuthProvider
               clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-              onFailure={handleFailure}
             >
-              <button
-                type="button"
-                className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                onClick={handleSuccess}
-              >
-                <FcGoogle className="mr-4" /> Sign in with Google
-              </button>
+              <GoogleLogin
+                render={(renderProps) => (
+                  <button
+                    type="button"
+                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                  >
+                    <FcGoogle className="mr-4" /> Sing in with Google
+                  </button>
+                )}
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy="single_host_origin"
+              />
             </GoogleOAuthProvider>
           </div>
         </div>
--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import App from "./App";
+import "./index.css";
 
-import App from './App';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Suspend rendering until we have enough information to make a confident update.
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Router>
         <App />
     </Router>
 );
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+
+// import App from './App';
+// import './index.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// // Suspend rendering until we have enough information to make a confident update.
+// root.render(
+//     <Router>
+//         <App />
+//     </Router>
+// );
 
 
 
